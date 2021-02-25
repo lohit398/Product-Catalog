cc8b21b468bf80ffd9621ec66041e95ca4ce9bef
Lohit <lohitkrishna39@gmail.com>
2021-02-26 01:38:54 +0530

    blueprint how it looks in mobile

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', () => {
     fetch('https://product-catalog-babayaga.herokuapp.com/products')
         .then(response => response.json())
-        .then(data => console.log(data)); // Products are retrieved
+        .then(data => {
+            console.log(data);
+        });
 })
