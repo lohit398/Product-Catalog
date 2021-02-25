c0d22eee8040df7c299d3ec2e757dadabf68b256
Lohit <lohitkrishna39@gmail.com>
2021-02-25 23:26:57 +0530

    CORS for localHost added

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ con.on('open', () => {
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 
 const productsRouter = require(__dirname + '/routes/products');
 app.use('/products', productsRouter);
